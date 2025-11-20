4bd39583d8330c8bcdcc201c7c5bb4eae0301aa6
carlosbd12 <carlos.cc12@hotmail.com>
2025-11-20 17:35:38 +0100

    ok

--- src/main/java/org/example/datasensefx/Main.java
+++ src/main/java/org/example/datasensefx/Main.java
@@ -1,8 +1,6 @@
 package org.example.datasensefx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.datasensefx.utils.SceneManager;
 
@@ -10,18 +8,9 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    /*public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("DataSense v1.0");
-        stage.setMinWidth(600);
-        stage.setMinHeight(500);
-        stage.setScene(scene);
-        stage.show();
-    }*/
 
     public void start(Stage stage) throws IOException {
-        SceneManager.setPrimaryStage(stage);
+        //SceneManager.setPrimaryStage(stage);
         SceneManager.switchScene("/org/example/datasensefx/views/login-view.fxml", "DataSense - Iniciar Sesión", 500, 650);
     }
     public static void main(String[] args) {
